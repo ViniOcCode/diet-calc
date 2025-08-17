a0e826915e091a65ff2bfe99cb4eb20efb20a95f
vinicius <viniciusoctavio@hotmail.com>
2025-08-17 16:36:12 -0300

    fix: goal error fix

--- app/main/routes.py
+++ app/main/routes.py
@@ -4,11 +4,11 @@ from app.calculator import Person, ValidationError
 from app.main import bp
 from .forms import PersonForm
 
-
 @bp.route('/', methods=['GET', 'POST'])
 def calculate():
     form = PersonForm()
     result = None
+    goal = None
 
     if form.validate_on_submit():
         goal = form.goal.data
